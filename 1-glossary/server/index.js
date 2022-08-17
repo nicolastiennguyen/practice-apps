2140a62e2bd0375c035a8390dc65b5f4c3ea6815
Nick <nicolastiennguyen@gmail.com>
2022-08-17 11:04:16 -0700

    successfully listening to server

--- 1-glossary/server/index.js
+++ 1-glossary/server/index.js
@@ -1,19 +1,19 @@
 require("dotenv").config();
 const express = require("express");
-const path = require("path");
-
 const app = express();
+const path = require("path");
+const controllers = require('./controllers/words.js')
+const PORT = 3000 || process.env.PORT
 
 // Serves up all static and generated assets in ../client/dist.
 app.use(express.static(path.join(__dirname, "../client/dist")));
+app.use(express.json());
 
-/**** 
- * 
- * 
- * Other routes here....
- *
- * 
- */
+// routes:
+app.get('/glossary', (req, res) => {
+  console.log('route is working')
+  controllers.get(req, res);
+});
 
-app.listen(process.env.PORT);
-console.log(`Listening at http://localhost:${process.env.PORT}`);
+app.listen(PORT);
+console.log(`Listening at http://localhost:${PORT}`);

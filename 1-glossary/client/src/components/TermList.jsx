ce96c26caa630deadb9c2b533b0d9a45495ecffb
Nick <nicolastiennguyen@gmail.com>
2022-08-18 21:00:11 -0700

    feat: click on term to enter edit mode

--- 1-glossary/client/src/components/TermList.jsx
+++ 1-glossary/client/src/components/TermList.jsx
@@ -5,13 +5,39 @@ import Delete from './Delete.jsx'
 class TermList extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {}
+    this.state = {
+      inEditMode: false
+    }
+    this.changeEditMode = this.changeEditMode.bind(this)
+    this.updateTerm = this.updateTerm.bind(this)
+  }
+
+  changeEditMode = () => {
+    this.setState({
+      inEditMode: !this.state.inEditMode
+    })
+  }
+
+  updateTerm = () => {
+    this.setState({
+      inEditMode: false
+    })
   }
 
   render() {
     let terms = this.props.DisplayedTerms.map((term) => {
-      return <div>{term.word + ': ' + term.definition}</div>
+      return this.state.inEditMode ?
+      <div>
+        <input type ="text"/>
+        <button onClick={this.changeEditMode}>X</button>
+        <button onClick={this.updateTerm}>OK</button>
+      </div>
+      :
+      <div onClick={this.changeEditMode}>
+        {term.word + ': ' + term.definition}
+      </div>
     })
+
     return (
       <div>
       {terms}

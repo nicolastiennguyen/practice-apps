72c541c737a3be76d5d42745775a86c7da516385
Nick <nicolastiennguyen@gmail.com>
2022-08-20 13:53:26 -0700

    fix: backticks for readability

--- 2-checkout/server/db.js
+++ 2-checkout/server/db.js
@@ -16,7 +16,21 @@ db.connectAsync()
   .then(() =>
     // Expand this table definition as needed:
     db.queryAsync(
-      "CREATE TABLE IF NOT EXISTS responses (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, name VARCHAR(50) NOT NULL, email VARCHAR(50) NOT NULL, password VARCHAR(50) NOT NULL, address1 VARCHAR (100) NOT NULL, address2 VARCHAR (100), city VARCHAR(50) NOT NULL, state VARCHAR(50) NOT NULL, zip VARCHAR(5) NOT NULL, phone VARCHAR(10) NOT NULL, creditcard VARCHAR(16) NOT NULL, expiry VARCHAR(4) NOT NULL, cvv VARCHAR(3) NOT NULL, billingzip VARCHAR(5) NOT NULL)"
+      `CREATE TABLE IF NOT EXISTS responses (
+        id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        name VARCHAR(50) NOT NULL,
+        email VARCHAR(50) NOT NULL,
+        password VARCHAR(50) NOT NULL,
+        address1 VARCHAR (100) NOT NULL,
+        address2 VARCHAR (100),
+        city VARCHAR(50) NOT NULL,
+        state VARCHAR(50) NOT NULL,
+        zip VARCHAR(5) NOT NULL,
+        phone VARCHAR(10) NOT NULL,
+        creditcard VARCHAR(16) NOT NULL,
+        expiry VARCHAR(4) NOT NULL,
+        cvv VARCHAR(3) NOT NULL,
+        billingzip VARCHAR(5) NOT NULL)`
     )
   )
   .catch((err) => console.log(err));
